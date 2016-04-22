6a669ded41069757142328c87f4b311d5eadfe0f
Jesus V <esuv38@gmail.com>
2016-04-22 12:13:03 +0200

    Index.php terminado

--- index.php
+++ index.php
@@ -10,6 +10,10 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
+        <form method="get" action="controladores/Controlador.php">
+         Saludo <input type="text" name="saludo"/><br/>
+        <input type="submit"  value="Enviar saludo"/>
+        
         <?php
         // put your code here
         ?>
